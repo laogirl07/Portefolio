155730862ef9e4235c89f23fcfc718bb4a88f4e2
laogirl07 <96246033+laogirl07@users.noreply.github.com>
2022-02-05 11:28:35 +0100

    Add files via upload

---
+++ Porte folio/dev/Javascript/script1.js
@@ -0,0 +1,10 @@
+/*
+const aujourdhui = new Date();
+console.log(aujourdhui);
+const jour = aujourdhui.getDay();
+console.log(jour);
+
+//Janvier = 0, février = 1...
+const mois = aujourdhui.getMonth();
+console.log(mois);
+/*
